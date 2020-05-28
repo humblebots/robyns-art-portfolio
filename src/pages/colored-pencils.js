eb9ad8125efa509ba794acb467ed4faa7ec6b988
differsthecat <nickersthecat@gmail.com>
2020-05-27 21:34:02 -0400

    Add images and start building medium pages

--- src/pages/colored-pencils.js
+++ src/pages/colored-pencils.js
@@ -4,32 +4,24 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
+import griffin from '../assets/images/colored_pencil/Griffin.jpeg'
+import kraken from '../assets/images/colored_pencil/Kraken.jpeg'
+import blueberries from '../assets/images/colored_pencil/Blueberries.jpeg'
 
-const Landing = (props) => (
+const ColoredPencils = (props) => (
     <Layout>
         <Helmet>
-            <title>Landing - Forty by HTML5 UP</title>
+            <title>Colored Pencils</title>
             <meta name="description" content="Landing Page" />
         </Helmet>
 
         <BannerLanding />
 
         <div id="main">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
-                </div>
-            </section>
             <section id="two" className="spotlights">
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={pic08} alt="" />
+                        <img src={griffin} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
@@ -45,7 +37,7 @@ const Landing = (props) => (
                 </section>
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={pic09} alt="" />
+                        <img src={kraken} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
@@ -61,7 +53,7 @@ const Landing = (props) => (
                 </section>
                 <section>
                     <Link to="/generic" className="image">
-                        <img src={pic10} alt="" />
+                        <img src={blueberries} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
@@ -81,4 +73,4 @@ const Landing = (props) => (
     </Layout>
 )
 
-export default Landing
+export default ColoredPencils
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Robyn's Illustrations"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
@@ -29,48 +29,48 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
                     <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
+                        <article style={{ backgroundImage: `url(${pic01})` }}>
                             <header className="major">
-                                <h3>Aliquam</h3>
-                                <p>Ipsum dolor sit amet</p>
+                                <h3>Gouache</h3>
+                                <p>Holbein Acryla</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
+                        <article style={{ backgroundImage: `url(${pic02})` }}>
                             <header className="major">
-                                <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
+                                <h3>Colored Pencil</h3>
+                                <p>Caran d'Ache Luminance / Primsacolors</p>
+                            </header>
+                            <Link to="/colored-pencils" className="link primary"></Link>
+                        </article>
+                        <article style={{ backgroundImage: `url(${pic03})` }}>
+                            <header className="major">
+                                <h3>Acrylic</h3>
+                                <p>Various brands</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic03})`}}>
+                        <article style={{ backgroundImage: `url(${pic04})` }}>
                             <header className="major">
-                                <h3>Magna</h3>
-                                <p>Lorem etiam nullam</p>
+                                <h3>Other</h3>
+                                <p>Oil paint, embroidery</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
-                            <header className="major">
-                                <h3>Ipsum</h3>
-                                <p>Nisl sed aliquam</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic05})`}}>
+                        {/* <article style={{ backgroundImage: `url(${pic05})` }}>
                             <header className="major">
                                 <h3>Consequat</h3>
                                 <p>Ipsum dolor sit amet</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic06})`}}>
+                        <article style={{ backgroundImage: `url(${pic06})` }}>
                             <header className="major">
                                 <h3>Etiam</h3>
                                 <p>Feugiat amet tempus</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
-                        </article>
+                        </article> */}
                     </section>
                     <section id="two">
                         <div className="inner">
